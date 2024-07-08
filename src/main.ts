db1fac0cc56c6eb1f65ae57f55afd95c66005d66
Holovko Maksym <golovkomaksim852@gmail.com>
2024-07-08 21:07:43 +0300

    ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,21 @@
-import './style.css'
+import "./style.css";
 
+function countProps(object: any): number {
+  let propCount: number = 0;
+  const keys = Object.keys(object);
+  const values = [];
+
+  for (let key of keys) {
+    values.push(object[key]);
+    console.log(values);
+    propCount += 1;
+  }
+
+  console.log(propCount);
+
+  return propCount;
+}
+
+countProps({});
+countProps({ name: "Mango", age: 2 });
+countProps({ mail: "poly@mail.com", isOnline: true, score: 500 });
